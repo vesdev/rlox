f0a86a8dbed94dd1e6b2df95b9003e4aeb58d52e
vesdev <nidi.same@gmail.com>
2023-05-05 05:34:13 +0300

    flake

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "spambotsen dev env";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+  
+  outputs = {nixpkgs, flake-utils, rust-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            (rust-bin.stable.latest.default.override {
+              extensions = [ "rust-src" "rust-analyzer" ];
+            })
+            cargo
+            pkg-config
+            openssl
+          ];
+        };
+        formatter.x86_64-linux = legacyPackages.${system}.nixpkgs-fmt;
+      }
+    );
+
+}
